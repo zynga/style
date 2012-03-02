e7c9209ba240856f3391b6987d2b4cfe90ae0045
Chris Campbell <ccampbell@zynga.com>
2012-03-02 13:49:11 -0800

    cssConverter.js can now more easily be included by node scripts, whereas css2js.js is for running from the command line

--- css2js.js
+++ css2js.js
@@ -9,58 +9,6 @@ var
 		return argv[2];
 	}()),
 	css = require('fs').readFileSync(inFile, 'utf-8'),
-	parsedRules = (new (require('jscssp').CSSParser)()).parse(css).cssRules,
-	style = require('./style'),
-	outRules = {}
+	converter = require('./cssConverter')
 ;
-
-function outputRules() {
-	console.log('style.add(');
-	console.log(outRules);
-	console.log(');');
-	outRules = {};
-}
-
-function iterateOverDeclarations(declarations, outRule) {
-	var
-		i = -1,
-		len = declarations.length,
-		declaration
-	;
-	while (++i < len) {
-		declaration = declarations[i];
-		property = declaration.property;
-		value = declaration.valueText;
-		if (property && value) {
-			outRule[property] = value;
-		}
-	}
-}
-
-function iterateOverParsedRules(parsedRules) {
-	var
-		i = -1,
-		len = parsedRules.length,
-		parsedRule,
-		selector,
-		outRule
-	;
-	while (++i < len) {
-		parsedRule = parsedRules[i];
-		selector = parsedRule.mSelectorText;
-		if (selector) {
-			if (outRules[selector]) {
-				// We already have a rule with the same selector.
-				outputRules();
-			}
-			outRule = outRules[selector] = {};
-			iterateOverDeclarations(parsedRule.declarations || [], outRule);
-		}
-	}
-}
-
-iterateOverParsedRules(parsedRules);
-
-if (outRules !== {}) {
-	outputRules();
-}
+console.log(converter.convert(css));
---
+++ cssConverter.js
@@ -0,0 +1,90 @@
+/*global document, module*/
+(function () {
+
+	// Establish the root object
+	var
+		root = this, // 'window' or 'global'
+		cssConverter = { VERSION: '0.0.1' },
+		previous = root.cssConverter
+	;
+	if (typeof module !== 'undefined' && module.exports) {
+		module.exports = cssConverter;
+	}
+	root.cssConverter = cssConverter;
+
+	cssConverter.noConflict = function () {
+		root.cssConverter = previous;
+		return this;
+	};
+
+	var parser = new (require('jscssp').CSSParser)();
+
+	function isEmpty(obj) {
+		for (var p in obj) {
+			if (obj.hasOwnProperty(p)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	cssConverter.convert = function (css) {
+		var
+			output = '',
+			outRules = {},
+			parsedRules = parser.parse(css).cssRules
+		;
+
+		function outputRules() {
+			output += 'style.add(' + JSON.stringify(outRules) + ');\n';
+			outRules = {};
+		}
+
+		function iterateOverDeclarations(declarations, outRule) {
+			var
+				i = -1,
+				len = declarations.length,
+				declaration
+			;
+			while (++i < len) {
+				declaration = declarations[i];
+				property = declaration.property;
+				value = declaration.valueText;
+				if (property && value) {
+					outRule[property] = value;
+				}
+			}
+		}
+
+		function iterateOverParsedRules(parsedRules) {
+			var
+				i = -1,
+				len = parsedRules.length,
+				parsedRule,
+				selector,
+				outRule
+			;
+			while (++i < len) {
+				parsedRule = parsedRules[i];
+				selector = parsedRule.mSelectorText;
+				if (selector) {
+					if (outRules[selector]) {
+						// We already have a rule with the same selector.
+						outputRules();
+					}
+					outRule = outRules[selector] = {};
+					iterateOverDeclarations(parsedRule.declarations || [], outRule);
+				}
+			}
+		}
+
+		iterateOverParsedRules(parsedRules);
+
+		if (!isEmpty(outRules)) {
+			outputRules();
+		}
+
+		return output;
+	};
+
+}());
